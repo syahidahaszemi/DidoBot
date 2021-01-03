0569a04d558722e9048bb45b719411568695ebc1
Syahidah Aszemi <n.junko41@gmail.com>
2021-01-03 20:33:04 +0800

    new update

--- index.js
+++ index.js
@@ -28,30 +28,14 @@ const conn = mysql.createConnection({
     password: "68747ae9",
     database: "heroku_5be77b62e4f10d6"
 })
-function handleDisconnect() {
-                                                    // the old one cannot be reused.
-  
-    conn.connect(function(err) {              // The server is either down
-      if(err) {                                     // or restarting (takes a while sometimes).
-        console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-      }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
-    conn.on('error', function(err) {
-      console.log('db error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-      } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                  // server variable configures this)
-      }
-    });
-  }
-  
-  handleDisconnect();
+
 dbcon();
 function dbcon(){
     console.log("Connected !");
+
+    setInterval(function () {
+        db.query('SELECT 1');
+    }, 5000);
     conn.query("SELECT * FROM gettoknow", function (err, result, fields){
         if(err){
             throw err;
