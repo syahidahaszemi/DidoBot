95a46198b9ce8d8e6ae300760c56e6a3cec37f5e
Syahidah Aszemi <n.junko41@gmail.com>
2021-01-23 23:08:59 +0800

    new updated for now

--- index.js
+++ index.js
@@ -719,7 +719,7 @@ bot.command('borak', ctx => {
         reply_markup: {
             inline_keyboard: [
                 [
-                    { text: '1', callback_data: '1'},
+                    { text: 'Fakta derpesi', callback_data: '1'},
                     { text: '2', callback_data: '2'}
                 ],
                 [
