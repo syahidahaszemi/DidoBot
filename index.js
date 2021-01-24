8e2f047bbe2f1c5f5efc6eab323b467a17a5fd20
Syahidah Aszemi <n.junko41@gmail.com>
2021-01-24 20:07:37 +0800

    updated

--- index.js
+++ index.js
@@ -719,7 +719,7 @@ bot.command('borak', ctx => {
         reply_markup: {
             inline_keyboard: [
                 [
-                    { text: 'Fakta depresi', callback_data: '1'},
+                    { text: '1', callback_data: '1'},
                     { text: '2', callback_data: '2'}
                 ],
                 [
